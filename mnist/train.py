dc83a3326432d05d2c2b1b9cb6d64193d08d9e6f
exgphe <exgphe@gmail.com>
2023-04-18 14:01:30 +0200

    Add MPS device

--- mnist/train.py
+++ mnist/train.py
@@ -30,7 +30,12 @@ def main(args):
     logger = create_logger(log_folder, 'train', 'info')
     print_args(args, logger)
 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    if torch.backends.mps.is_available():
+        device = torch.device("mps")
+    elif torch.cuda.is_available():
+        device = torch.device("cuda")
+    else:
+        device = torch.device("cpu")
     torch.manual_seed(args.seed)
     if torch.cuda.is_available():
         torch.cuda.manual_seed(args.seed)
